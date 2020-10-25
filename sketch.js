3c0457ad320d094879f103fa14a42f21c53031e3
shreeya578 <shreeya.garg13@gmail.com>
2020-10-25 12:25:36 +0530

    contraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constraintLog=new Log(100,200,80,PI/2);
+    chain=new Chain(bird.body,constraintLog.body);
 
 }
 
@@ -61,4 +65,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constraintLog.display();
+    chain.display();
+
 }
